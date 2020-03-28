6026634c03b7cfe6f72b3ec7863777936f2c1544
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-03-28 12:45:30 +0530

    search button and differntiator done

--- src/location.js
+++ src/location.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import './location.css'
 
 class Location extends Component{
-    handleClick = ()=>{
+    state = {
+        latitude: undefined,
+        longitude: undefined,
+        restaurants: []
+    }
+    handleLocation = ()=>{
         if (navigator.geolocation){
             navigator.geolocation.getCurrentPosition(function(position){
                 let {latitude, longitude} = position.coords;
@@ -13,12 +18,18 @@ class Location extends Component{
             alert('Geolocation is not supported for your browser. Please enter the location manually');
         }
     }
+    handleRestaurantSearch = ()=>{
+
+    }
     render(){
         return (
             <div className="location-conatiner">
                 <input type="text" id="lat" className="location-input" placeholder="Latitude"/>
                 <input type="text" id="long" className="location-input" placeholder="Longitude"/>
-                <button onClick = {this.handleClick} className="findme-button">DETECT MY LOCATION</button>
+                <button onClick = {this.handleLocation} className="findme-button">DETECT MY LOCATION</button>
+                <button onClick = {this.handleRestaurantSearch} className="search-button">FIND RESTAURANTS</button>
+                <br/>
+                <hr className="divider"/>
             </div>
         );
     }
