1c063330329cbcee4103ecec5b2abbc6fb8377e0
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-03-28 19:20:12 +0530

    added loader

--- src/location.js
+++ src/location.js
@@ -2,20 +2,21 @@ import React, {Component} from 'react';
 import './location.css'
 import axios from 'axios';
 import Restaurants from './restaurants.js'
+import loader from './loader.gif'
 
 class Location extends Component{
     state = {
-        latitude: undefined,
-        longitude: undefined,
         restaurants: [],
         userSearch: false
     }
     handleLocation = ()=>{
         if (navigator.geolocation){
+            document.getElementById('loader').style.display = '';
             navigator.geolocation.getCurrentPosition(function(position){
                 let {latitude, longitude} = position.coords;
                 document.getElementById('lat').value = latitude;
                 document.getElementById('long').value = longitude;
+                document.getElementById('loader').style.display = 'none';
             });
         } else{
             alert('Geolocation is not supported for your browser. Please enter the location manually');
@@ -25,6 +26,8 @@ class Location extends Component{
         let lat = +document.getElementById('lat').value,
             lon = +document.getElementById('long').value,
             comp  =this;
+            document.getElementById('loader').style.display = '';
+            debugger;
         axios.get('https://developers.zomato.com/api/v2.1/search', { 
             params: { 
                     lat,
@@ -34,21 +37,29 @@ class Location extends Component{
              },
             headers: {'user-key': 'b53c4652dac57dca5ffe0764a570822a'}
             }).then(function(response){
+                debugger;
+            document.getElementById('loader').style.display = 'none';
             comp.setState({
-                restaurants: response.data.restaurants
+                restaurants: response.data.restaurants,
+                userSearch: true
             });
         });
     }
     render(){
         return (
-            <div className="location-conatiner">
-                <input type="text" id="lat" className="location-input" placeholder="Enter your Latitude"/>
-                <input type="text" id="long" className="location-input" placeholder="Enter your Longitude"/>
-                <button onClick = {this.handleLocation} className="findme-button">DETECT MY LOCATION</button>
-                <button onClick = {this.handleRestaurantSearch} className="search-button">FIND RESTAURANTS</button>
-                <br/>
-                <hr className="divider"/>
-                <Restaurants restaurants={this.state.restaurants}/>
+            <div className="main-container">
+                <div className="location-conatiner">
+                    <input type="text" id="lat" className="location-input" placeholder="Enter your Latitude"/>
+                    <input type="text" id="long" className="location-input" placeholder="Enter your Longitude"/>
+                    <button onClick = {this.handleLocation} className="findme-button">DETECT MY LOCATION</button>
+                    <button onClick = {this.handleRestaurantSearch} className="search-button">FIND RESTAURANTS</button>
+                    <br/>
+                    <hr className="divider"/>
+                    <Restaurants restaurants={this.state.restaurants}/>
+                </div>
+                <div id="loader" style={{display: 'none'}}>
+                    <img className="loader-gif" src={loader} alt="loader-gif"></img>
+                </div>
             </div>
         );
     }
